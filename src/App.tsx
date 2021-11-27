f3e7f17c3914ca0279c565d5e57c0626fd4ca16e
Danpa_cho <danpa725@cau.ac.kr>
2021-11-27 17:36:51 +0900

    Lesson1: AppLoading procedure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function App() {
+	const [isLoading, setLoading] = useState(true);
+
+	const onFinish = () => setLoading(false);
+
+	const startAsyncLoading = async () => {
+		//* 앱 시작 전, 미리 할 작업 실행
+		await new Promise((resolve) => setTimeout(resolve, 10000));
+	};
+
+	if (isLoading)
+		//* 로딩 화면
+		return (
+			<AppLoading
+				onFinish={onFinish}
+				startAsync={startAsyncLoading}
+				onError={console.warn}
+			/>
+		);
+
 	return (
+		//* 로딩 종료 시 컴포넌트
 		<View>
-			<Text>Typescript setup</Text>
+			<Text>Loading Finished!</Text>
 		</View>
 	);
 }
