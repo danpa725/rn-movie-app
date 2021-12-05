a3ca272036e927dadc185ffd4bb34b670623c47f
Danpa_cho <danpa725@cau.ac.kr>
2021-12-05 15:38:46 +0900

    Make: swiper component.

--- src/components/view/screen/Movie/MovieScreen.tsx
+++ src/components/view/screen/Movie/MovieScreen.tsx
@@ -1,8 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, View } from "react-native";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import pallete from "@/utils/style/pallete";
 
+const Container = styled.ScrollView`
+    flex: 1;
+    background: ${(props) => props.theme.background};
+`;
+
+const MovieCard = styled.View`
+    flex: 1;
+    background: ${pallete.blue3};
+`;
 function MovieScreen() {
-    return <View></View>;
+    const { height } = Dimensions.get("window");
+
+    return (
+        <Container>
+            <Swiper
+                containerStyle={{
+                    width: "100%",
+                    height: height / 4,
+                }}
+                loop={true}
+                timeout={3.25}
+                controlsEnabled={false}
+            >
+                <MovieCard></MovieCard>
+                <MovieCard></MovieCard>
+                <MovieCard></MovieCard>
+                <MovieCard></MovieCard>
+            </Swiper>
+        </Container>
+    );
 }
 
 export default MovieScreen;
