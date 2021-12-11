a34da66f88cdf5d63146546e11f0c4326c63264b
Danpa_cho <danpa725@cau.ac.kr>
2021-12-11 16:26:20 +0900

    Make: refreshing data with RefreshControl component.

--- src/components/view/screen/Movie/MovieScreen.tsx
+++ src/components/view/screen/Movie/MovieScreen.tsx
@@ -1,32 +1,42 @@
 import React, { useEffect, useState } from "react"
-import styled from "styled-components/native"
+import { RefreshControl } from "react-native"
+
+import { MovieData, TrendingMovieData } from "./utils/interface"
 import { getMovieList } from "./utils/getMovieData"
 
-import Loader from "@/components/atoms/Loader/Loader"
+import Loader from "@components/atoms/Loader/Loader"
+
 import MovieSlider from "./MovieSlider/MovieSlider"
-import { MovieData, TrendingMovieData } from "./utils/interface"
-import { MovieScreenContainer } from "./MovieScreen.style"
 import MovieTrendingList from "./MovieTrendingList/MovieTrendingList"
+import MovieUpcomingList from "./MovieUpcomingList/MovieUpcomingList"
+
+import { MovieScreenContainer } from "./MovieScreen.style"
 
 function MovieScreen() {
+    //* loading state
     const [loading, setLoading] = useState(true)
+
+    //* data state
     const [movieList, setMovieList] = useState<MovieData[]>([])
     const [upcomingMovieList, setUpcomingMovieList] = useState<MovieData[]>([])
     const [trendingMovieList, setTrendingMovieList] = useState<
         TrendingMovieData[]
     >([])
 
-    const fetchData = () => {
-        Promise.resolve(getMovieList("now_playing")).then((data: MovieData[]) =>
-            setMovieList(data)
+    //* fetching data
+    const fetchData = async () => {
+        const movieList = await Promise.resolve(getMovieList("now_playing"))
+        setMovieList(movieList)
+
+        const upcomingMovieList = await Promise.resolve(
+            getMovieList("upcoming")
         )
-        Promise.resolve(getMovieList("upcoming")).then((data: MovieData[]) =>
-            setUpcomingMovieList(data)
+        setUpcomingMovieList(upcomingMovieList)
+
+        const trendingMovieList = await Promise.resolve(
+            getMovieList("trending")
         )
-        Promise.resolve(getMovieList("trending")).then(
-            (data: TrendingMovieData[]) =>
-                setTrendingMovieList(data.slice(0, 9))
-        )
+        setTrendingMovieList(trendingMovieList.slice(0, 9))
     }
 
     useEffect(() => {
@@ -34,12 +44,25 @@ function MovieScreen() {
         setLoading(false)
     }, [])
 
+    //* refreshing
+    const [refreshing, setRefreshing] = useState(false)
+    const onRefresh = async () => {
+        setRefreshing(true)
+        await fetchData()
+        setRefreshing(false)
+    }
+
     if (loading) return <Loader />
 
     return (
-        <MovieScreenContainer>
+        <MovieScreenContainer
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+        >
             <MovieSlider movieList={movieList} />
             <MovieTrendingList trendingMovieList={trendingMovieList} />
+            <MovieUpcomingList upcomingMovieList={upcomingMovieList} />
         </MovieScreenContainer>
     )
 }
